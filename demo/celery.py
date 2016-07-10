6103466de6b98ed2ef2a90f4629aa914ed2c9bd1
buendiya <jingshuaizhen@gmail.com>
2016-07-11 07:30:05 +0800

    update

--- demo/celery.py
+++ demo/celery.py
@@ -3,6 +3,8 @@ import os
 
 from celery import Celery
 
+from demo.CONFIGS import BROKER_URL
+
 app = Celery('tasks', 
-             broker=os.environ.get('celery_demo_broker', "amqp://@amqp"))
+             broker=os.environ.get('celery_demo_broker', BROKER_URL))
 app.conf.CELERY_ALWAYS_EAGER = True
--- test.py
+++ test.py
@@ -3,4 +3,5 @@ from __future__ import absolute_import
 from demo.tasks import add
 
 if __name__ == '__main__':
-    print add.delay(1,2)
+    result = add.delay(1,2)
+    print result.get()
