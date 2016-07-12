8078539ce63945d3c17ad9f124f5f73c4033193f
jingsz <jingshuaizhen@haomaiyi.com>
2016-07-12 10:14:12 +0800

    update

--- demo/celeryconfig.py
+++ demo/celeryconfig.py
@@ -1,2 +1,18 @@
+from __future__ import absolute_import
+
+from celery.schedules import crontab
+
+name = 'demo'
+
+
+CELERYBEAT_SCHEDULE = {
+                       'add_task': {'task': '%s.tasks.add' % name,
+                                                    'args': (1, 2),
+                                                    'schedule': crontab(hour=11, minute=24),
+                                                },
+                       }
+
+CELERY_TIMEZONE = 'Asia/Shanghai'
+
 
 CELERY_ALWAYS_EAGER = True
