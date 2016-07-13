72e6fb1b29fbe01a6c8cb003bb2b3a079fa22ce1
buendiya <jingshuaizhen@gmail.com>
2016-07-13 21:43:46 +0800

    update

--- demo/tasks.py
+++ demo/tasks.py
@@ -3,6 +3,17 @@ from __future__ import absolute_import
 from demo.celery import app
 from celery.utils.log import get_task_logger
 
+# @app.task
+# class add(object):
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#     
+#     def __call__(self):
+#         print 'hello'
+#         print __name__
+#         return self.x + self.y
+
 @app.task
 def add(x, y):
     print 'hello'
--- test.py
+++ test.py
@@ -3,5 +3,4 @@ from __future__ import absolute_import
 from demo.tasks import add
 
 if __name__ == '__main__':
-    result = add.delay(1,2)
-    print result.get()
+    add.delay(2,2)
