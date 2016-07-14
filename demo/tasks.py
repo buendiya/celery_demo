00f8ea7d49d60fda7fa9ab97872f19bd84499e1e
buendiya <jingshuaizhen@gmail.com>
2016-07-14 08:53:01 +0800

    update

--- demo/tasks.py
+++ demo/tasks.py
@@ -1,21 +1,33 @@
 from __future__ import absolute_import
 
+import json
+import random
+
+from logging.handlers import RotatingFileHandler
+
 from demo.celery import app
 from celery.utils.log import get_task_logger
+from .CONFIGS import DEBUG_FILE
 
-# @app.task
-# class add(object):
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#     
-#     def __call__(self):
-#         print 'hello'
-#         print __name__
-#         return self.x + self.y
+logger = get_task_logger(__name__)
+file_handler = RotatingFileHandler(DEBUG_FILE)
+logger.addHandler(file_handler)
 
-@app.task
-def add(x, y):
-    print 'hello'
-    print __name__
+
+@app.task(bind=True)
+def add(self, x, y):
+    # logger.info(json.dumps(self.request.__dict__, indent=4))
+    logger.info('hello, world')
     return x + y
+
+
+@app.task(bind=True)
+def retry_demo_task(self):
+    try:
+        random_value = random.randrange(0, 2)
+        logger.info('I am coming into retry with %s' % random_value)
+        if random_value == 1:
+            raise Exception('I am retry Exception')
+        logger.info('I have finished retry.')
+    except Exception as exc:
+        raise self.retry(countdown=1, exc=exc)
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from __future__ import absolute_import
 
-from demo.tasks import add
+from demo.tasks import add, retry_demo_task
+
+
+def test_add():
+    add.delay(2,2)
+
+
+def test_retry():
+    retry_demo_task.delay()
+    
 
 if __name__ == '__main__':
-    add.delay(2,2)
+    test_add()
